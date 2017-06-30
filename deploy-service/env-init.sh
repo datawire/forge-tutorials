1523920c79b0d4d146b8e55dd9f99f3230dc7ec5
Richard Li <richard@datawire.io>
2017-06-30 14:11:46 -0400

    set up env

--- deploy-service/env-init.sh
+++ deploy-service/env-init.sh
@@ -1,1 +1,2 @@
-git clone https://github.com/katacoda/scenario-example
+curl -sL https://raw.githubusercontent.com/datawire/forge/master/install.sh | INSTALL_DIR=${HOME}/forge sh
+git clone https://github.com/datawire/hello-forge.git

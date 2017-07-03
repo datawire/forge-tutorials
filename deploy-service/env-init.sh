78d56a747cf613aae753fc667fad51fdd5aa859c
Richard Li <richard@datawire.io>
2017-07-03 14:23:05 -0400

    make all commands on one line which seems to fix things

--- deploy-service/env-init.sh
+++ deploy-service/env-init.sh
@@ -1,7 +1,5 @@
 apt-get install -y python-minimal python-pip \
-&& echo "installing virtualenv" \
-&& pip install virtualenv
-echo "installing forge"
-curl -sL https://raw.githubusercontent.com/datawire/forge/master/install.sh | INSTALL_DIR=${HOME}/forge sh
-export PATH=$PATH:${HOME}/forge/bin
-git clone https://github.com/datawire/hello-forge.git
+&& pip install virtualenv \
+&& curl -sL https://raw.githubusercontent.com/datawire/forge/master/install.sh | INSTALL_DIR=${HOME}/forge sh \
+&& export PATH=$PATH:${HOME}/forge/bin \
+&& git clone https://github.com/datawire/hello-forge.git

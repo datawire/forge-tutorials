857bd691f31b020bc181d8bbc88459611f8052a6
Richard Li <richard@datawire.io>
2017-07-03 13:02:20 -0400

    add echo

--- deploy-service/env-init.sh
+++ deploy-service/env-init.sh
@@ -1,4 +1,5 @@
 apt-get install -y python-minimal python-pip
+echo "installing virtualenv"
 pip install virtualenv
 curl -sL https://raw.githubusercontent.com/datawire/forge/master/install.sh | INSTALL_DIR=${HOME}/forge sh
 export PATH=$PATH:${HOME}/forge/bin

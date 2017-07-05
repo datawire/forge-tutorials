4a0a766a6ad32e2100a208c64afb5f1f6e2f7f6b
Richard Li <richard@datawire.io>
2017-07-05 10:29:26 -0400

    switch repo

--- deploy-service/env-init.sh
+++ deploy-service/env-init.sh
@@ -2,4 +2,4 @@ apt-get install -y python-minimal python-pip \
 && pip install virtualenv \
 && curl -sL https://raw.githubusercontent.com/datawire/forge/master/install.sh | INSTALL_DIR=${HOME}/forge sh \
 && export PATH=$PATH:${HOME}/forge/bin \
-&& git clone https://github.com/datawire/hello-forge.git
+&& git clone https://github.com/datawire/hello-webapp.git

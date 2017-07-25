fa56abc202325fa31fcf1bdee3c13b9e21bbffaf
Richard Li <richard@datawire.io>
2017-07-25 10:28:30 -0400

    refactor env setup

--- deploy-service/env-init.sh
+++ deploy-service/env-init.sh
@@ -1,2 +1,3 @@
 export PATH=$PATH:~/.bin/forge/bin \
-&& git clone https://github.com/datawire/hello-webapp.git
+&& git clone https://github.com/datawire/hello-webapp.git \
+&& export FORGE_SETUP_IMAGE=registry.hub.docker.com/datawire/forge-setup-test-katacoda:1

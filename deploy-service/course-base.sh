6db88a961f1557feccff461f1ac888e9aaa77c85
Richard Li <richard@datawire.io>
2017-07-25 10:24:07 -0400

    switch setup image

--- deploy-service/course-base.sh
+++ deploy-service/course-base.sh
@@ -1,2 +1,3 @@
 curl -sL https://raw.githubusercontent.com/datawire/forge/master/install.sh | INSTALL_DIR=~/.bin/forge sh
+export FORGE_SETUP_IMAGE=registry.hub.docker.com/datawire/forge-setup-test-katacoda:1
 docker run -d -p 5000:5000 --name registry registry:2

76cc92af6f8e05096a1e613f2792639df15bff28
Richard Li <richard@datawire.io>
2017-08-04 16:35:48 -0400

    generate uuid

--- deploy-service/env-init.sh
+++ deploy-service/env-init.sh
@@ -1,3 +1,4 @@
 export PATH=$PATH:~/.bin/forge/bin \
 && git clone https://github.com/datawire/hello-webapp.git \
-&& export FORGE_SETUP_IMAGE=registry.hub.docker.com/datawire/forge-setup-test-katacoda:1
+&& export FORGE_SETUP_IMAGE=registry.hub.docker.com/datawire/forge-setup-test-katacoda:1 \
+&& cat /proc/sys/kernel/random/uuid > hello-webapp/uuid.txt

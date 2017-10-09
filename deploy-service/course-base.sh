1e9cad18d21db9c41194febc419226cc426ff73a
Richard Li <richard@datawire.io>
2017-10-09 10:23:39 -0400

    forgot to move binary

--- deploy-service/course-base.sh
+++ deploy-service/course-base.sh
@@ -1,4 +1,4 @@
 curl https://s3.amazonaws.com/datawire-static-files/forge/$(curl https://s3.amazonaws.com/datawire-static-files/forge/latest.url)/forge?x-download=katacoda -o /tmp/forge
-
-curl -sL https://raw.githubusercontent.com/datawire/forge/master/install.sh | INSTALL_DIR=~/.bin/forge sh
+chmod a+x /tmp/forge
+mv /tmp/forge ~/.bin
 docker run -d -p 5000:5000 --name registry registry:2

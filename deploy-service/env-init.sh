cb3d84c3afcdd40f9c2e5e83f3bc26147b593ccc
Richard Li <richard@datawire.io>
2017-10-09 10:16:45 -0400

    switch to pex and add env variables for forge 0.3

--- deploy-service/env-init.sh
+++ deploy-service/env-init.sh
@@ -1,4 +1,5 @@
 export PATH=$PATH:~/.bin/forge/bin \
 && git clone https://github.com/datawire/hello-webapp.git \
 && export FORGE_SETUP_IMAGE=registry.hub.docker.com/datawire/forge-setup-test-katacoda:1 \
-&& cat /proc/sys/kernel/random/uuid > hello-webapp/uuid.txt
+&& cat /proc/sys/kernel/random/uuid > hello-webapp/uuid.txt \
+&& export LANG=C.UTF-8 && export LC_ALL=C.UTF-8

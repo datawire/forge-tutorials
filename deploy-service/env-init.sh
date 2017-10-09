045ee2d21ab8f0d1d1cc5d18fffe7fc2579c73af
Richard Li <richard@datawire.io>
2017-10-09 10:19:35 -0400

    fix path

--- deploy-service/env-init.sh
+++ deploy-service/env-init.sh
@@ -1,4 +1,4 @@
-export PATH=$PATH:~/.bin/forge/bin \
+export PATH=$PATH:~/.bin/ \
 && git clone https://github.com/datawire/hello-webapp.git \
 && export FORGE_SETUP_IMAGE=registry.hub.docker.com/datawire/forge-setup-test-katacoda:1 \
 && cat /proc/sys/kernel/random/uuid > hello-webapp/uuid.txt \

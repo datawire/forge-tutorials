4ae9f1bbe095f6cdc611a1face754f0e35646b74
Richard Li <richard@datawire.io>
2017-10-16 12:35:52 -0400

    add tracking link

--- deploy-service/course-base.sh
+++ deploy-service/course-base.sh
@@ -1,4 +1,4 @@
-curl https://s3.amazonaws.com/datawire-static-files/forge/$(curl https://s3.amazonaws.com/datawire-static-files/forge/latest.url)/forge?x-download=katacoda -o /tmp/forge
+curl https://s3.amazonaws.com/datawire-static-files/forge/$(curl https://s3.amazonaws.com/datawire-static-files/forge/latest.url?x-download=katacoda)/forge?x-download=katacoda -o /tmp/forge
 chmod a+x /tmp/forge
 mv /tmp/forge ~/.bin
 docker run -d -p 5000:5000 --name registry registry:2
